cee82525e403a89854bffbb79c931265f96e1e5b
Jimmy Sanders-Cannon <jimmysancan@yahoo.com>
2018-01-26 15:08:01 -0500

    working logic

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-const waterWalls = function(array) {
-  let water = [];
-  for (let i = 0 ; i < array.length; i++) {
-    if (array[i-1] > array[i] && array[i+1] > array[i]) {
-      water.push(Math.min(array[i-1], array[i+1]) - array[i])
-    }
-    if (array[i] > array[i+1]) {
-      
-    }
-    else {
-      water.push(0)
+
+findGreatestRun = arr => {
+  let returnObj = []
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length - 1; j++) {
+      let sum = sumBetween(i, j, arr)
+      returnObj[sum] = [i, j + 1]
     }
   }
-  return water
+  let highest = returnObj[returnObj.length - 1 ]
+  return [...highest, returnObj.length - 1] 
 }
+
+sumBetween = (i, j, arr) => arr.slice(i, j).reduce((a, e) => a + e, 0)
+
+let waterWalls = (array) => {
+  let waterDepths = [0]
+  for (let i = 1; i < array.length - 1; i++) {
+    let left = array.slice(0,i)
+    let right = array.slice(i+1)
+    let leftV = Math.max(...left)
+    let rightV = Math.max(...right)
+    let waterLevel = Math.min(leftV, rightV)
+    waterDepths.push(waterLevel - array[i])
+  }
+  return findGreatestRun(waterDepths)
+}
+
+console.log(waterWalls([5, 3, 7, 2, 6, 4, 5, 9, 1, 2]))
